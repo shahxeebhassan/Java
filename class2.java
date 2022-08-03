5f00ae2d1158f17551815e4f2478c57fe73fd36f
Shahzaib Hassan <shahxeebhassan@gmail.com>
2022-08-03 17:21:08 +0500

    Learned Output

--- class2.java
+++
@@ -1,6 +0,0 @@
-class class2 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
-// Language: java
---
+++ com/class2/output.java
@@ -0,0 +1,21 @@
+package com.class2;
+
+class output {
+    public static void main(String[] args) {
+        //Output in java
+        System.out.println("Hello World!");
+        System.out.println("My name is: " + "Shahzaib");
+        System.out.println("My age is: " + "20");
+        System.out.println("My height is: " + "5.8");
+        System.out.println("My weight is: " + "60");
+        System.out.println("My favorite food is: " + "Pizza");
+        System.out.println("My favorite color is: " + "Black");
+        System.out.println("My favorite animal is: " + "Parrot");
+        System.out.println("My favorite movie is: " + "The Dilwale");
+        System.out.println("My favorite song is: " + "Ae Dil Hai Mushkil");
+        System.out.println("My favorite sport is: " + "Cricket");
+        System.out.println("My favorite book is: " + "Holy Quran");
+        System.out.println("My favorite game is: " + "Pubg\n");
+        System.out.println("\n*\n**\n***\n****");                   
+    }
+}

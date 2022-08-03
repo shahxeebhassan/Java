9187cc2792ee312d4aaaf627f3f60bcbf18902ee
Shahzaib Hassan <shahxeebhassan@gmail.com>
2022-08-03 17:58:08 +0500

    Lec 2 Completed

---
+++ com/class2/variables.java
@@ -0,0 +1,32 @@
+package com.class2;
+
+public class variables {
+    public static void main(String[] args) {
+        // int a = 10;
+        // int b = 20;
+        // int c = a + b;
+        int a,b,c;
+        a = 10;
+        b = 20;
+        c = a + b;
+        System.out.println(c);
+
+        String name = "Shahzaib";
+        String name2 = "Tasha";
+        String life = name2 + "\n" + name;
+        System.out.println(life);
+
+        float f = 10.5f;
+        // double d = 10.5;
+        System.out.println(f);
+
+        boolean b1 = true;
+        // boolean b2 = false;
+        System.out.println(b1);
+
+        char c1 = 'A';
+        // char c2 = 'B';
+        // char c3 = 'C';
+        System.out.println(c1);
+    }
+}

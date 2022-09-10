1cd52f8633bfa4a6f11f7aaed340cc483d0d3794
Shahzaib Hassan <shahxeebhassan@gmail.com>
2022-09-10 22:17:04 +0500

    no msg

---
+++ com/class10/array1.java
@@ -0,0 +1,18 @@
+package com.class10;
+import java.util.*;
+public class array1 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int size=sc.nextInt();
+        int[] arr=new int[size];
+        System.out.println("Enter the elements of array");
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter the element to be searched");
+        int ele=sc.nextInt();
+    }
+}

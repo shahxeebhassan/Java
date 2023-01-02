cad3cd76fa9eb6867de7237f6a1a4e6ce9bb431b
Shahzaib Hassan <shahxeebhassan#gmail.com>
2023-01-02 09:45:30 +0500

    Style updated

--- TopicWise_Practice/Array/Reverse.java
+++ TopicWise_Practice/Array/Reverse.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class reverse {
+public class Reverse {
     public static void main(String[] args) {
         int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         rever(arr);

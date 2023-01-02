7e6f0f20d2d282aa3efb722bf4710405e8fdf97f
Shahzaib Hassan <shahxeebhassan#gmail.com>
2023-01-02 10:05:22 +0500

    Style improved

--- TopicWise_Practice/ExamPrep/Overloading/MainOverloading.java
+++ TopicWise_Practice/ExamPrep/Overloading/MainOverloading.java
@@ -1,7 +1,7 @@
 public class MainOverloading {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        MainOverloading mo = new MainOverloading();
+        // MainOverloading mo = new MainOverloading();
         MainOverloading.main("Shahzaib");
     }
     public static void main(String name) {
--- TopicWise_Practice/ExamPrep/Polymorphism.java
+++ TopicWise_Practice/ExamPrep/Polymorphism.java
@@ -2,13 +2,14 @@ class Animal{
     void eat(){System.out.println("animal is eating...");}
     }
     
-    class Dog extends Animal{
+class Dog extends Animal{
     void eat(){System.out.println("dog is eating...");}
-    }
+}
     
-    class BabyDog1 extends Dog{
+class BabyDog1 extends Dog{
     public static void main(String args[]){
     Animal a=new BabyDog1().eat();;
     // a.eat();
-    }}
+    }
+}
     

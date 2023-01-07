04f20d2100fd64cced0ef5964c3f8297b356ba2e
Shahzaib Hassan <shahxeebhassan#gmail.com>
2023-01-07 20:01:25 +0500

    Module 1

---
+++ Alpha/TypePromoExampl.java
@@ -0,0 +1,9 @@
+public class TypePromoExampl {
+    public static void main(String[] args) {
+        char a = 'b';
+        byte b = 1;
+        short c = 2;
+        short d = a + b + c; // char is promoted to int so gives error: incompatible types: possible lossy conversion from int to short
+        System.out.println(d);
+    }
+}
